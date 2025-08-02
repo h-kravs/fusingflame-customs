d102e0a89fe19de07b34f78dd5aff14d617cc43d
Facundo Utrico <info@facundo-utrico.com>
2025-08-02 17:21:53 -0300

    asd

--- web/README.md
+++
@@ -1,4 +0,0 @@
-Script made By Kravs
-
-# Discord/Support
-### [Future Script](https://discord.com/invite/4m8es2VJN8)
--- web/client/modules/store.lua
+++
@@ -1,9 +0,0 @@
-return {
-    menu = 'main',
-    modType = 'none',
-    parentMenu = 'none',
-    customizationType = 'none',
-    stored = {},
-    preview = false,
-    paintColorType = nil -- Track if we're modifying Primary or Secondary color
-}
