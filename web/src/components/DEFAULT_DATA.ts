35535644df641ab2bb533c95e17e60870ed3937f
Facundo Utrico <info@facundo-utrico.com>
2025-08-02 18:01:07 -0300

    test

--- web/src/components/DEFAULT_DATA.ts
+++
@@ -1,9 +0,0 @@
-import { MenuProps} from "./type";
-
-const default_data = { type: 'menu', card: {current: 'main', previous: 'main'}, data: [
-    {id: 'preview', selected: true, label: 'Preview', icon: 'car-side'},
-    {id: 'customization', label: 'Customization', icon: 'paint-brush'},
-    {id: 'performance', label: 'Performance', icon: 'screwdriver-wrench'},
-], currentMenu: 'main', mainMenus: [], submenuMenus: [], colorMenus: [], defaultMenu: []} as MenuProps
-
-export default default_data
